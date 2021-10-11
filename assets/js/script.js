63c8f444afee8712fbbfda20003b009155a3a171
Zabirraihan <zabirraihan570@gmail.com>
2021-10-11 19:40:29 +0600

    nav section desgin complete without mega menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,16 @@
 $(function(){
+
+    /* top  header slick */
     $('.slick_slider').slick({
         arrows:false,
         vertical:true,
         verticalSwiping:true,
         autoPlay:true,
+    })
+
+
+    /* nav section js */
+    $('.dropdown_item').on('click',function(){
+        $('.dropdown_list').addClass('.show')
     })
 })
