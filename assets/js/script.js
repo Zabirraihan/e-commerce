851444bf5b4e869f5e0a50a7ba49dcde85a6346b
Zabirraihan <zabirraihan570@gmail.com>
2021-10-12 10:16:42 +0600

    ecommerce mega menu and banner desgin

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,4 +13,11 @@ $(function(){
     $('.dropdown_item').on('click',function(){
         $('.dropdown_list').addClass('.show')
     })
+
+    // banner slick slider
+    $('.banner_section').slick({
+        autoplay:true,
+        nextArrow:'<i class="fas fa-chevron-right arrow_right"></i>',
+        prevArrow:'<i class="fas fa-chevron-left arrow_left"></i>,'
+    })
 })
