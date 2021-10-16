a22e14564df4e75f21092681904b109dc11bc9c7
Zabirraihan <zabirraihan570@gmail.com>
2021-10-16 14:04:25 +0600

    featured categories and product item desgin

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,10 +14,27 @@ $(function(){
         $('.dropdown_list').addClass('.show')
     })
 
+    $(window).on("scroll",function(){
+        var sticky = $(window).scrollTop();
+        if(sticky > 180){
+            $(".navbar_menu").addClass("sticky")
+        }else{
+            $(".navbar_menu").removeClass("sticky")
+        }
+    })
+
     // banner slick slider
     $('.banner_section').slick({
         autoplay:true,
-        nextArrow:'<i class="fas fa-chevron-right arrow_right"></i>',
-        prevArrow:'<i class="fas fa-chevron-left arrow_left"></i>,'
+        nextArrow:'<i class="fas fa-chevron-right slider_icon arrow_right"></i>',
+        prevArrow:'<i class="fas fa-chevron-left slider_icon arrow_left"></i>,'
+    })
+
+    //featured categories slick
+    $('.featured_categories_slider').slick({
+        slidesToShow: 10,
+        slidesToScroll: 3,
+        prevArrow: '<i class="fas fa-long-arrow-alt-right  slider_icon featured_icon_left"></i>',
+        nextArrow: '<i class="fas fa-long-arrow-alt-left slider_icon  featured_icon_right"></i>',
     })
 })
